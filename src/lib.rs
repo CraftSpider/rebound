0acf0a45290a38cb82738f6d48c770a51b247b6c
Rune Tynan <runetynan@gmail.com>
2021-12-09 16:52:49 -0500

    Test new way of handling impls

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,8 @@
 
 #[doc(hidden)]
 pub use rebound_proc::rebound;
+#[doc(hidden)]
+pub use rebound_proc::post_wire;
 
 pub use error::Error;
 pub use info::{AssocConst, AssocFn, Field, FieldKind, UnionField, Variant};
